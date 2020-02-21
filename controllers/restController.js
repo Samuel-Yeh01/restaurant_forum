0a5b3405eaf364b812ddb650bfffb3f152f18424
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-02-21 17:16:02 +0800

    add User Profile as S3A6 submission

--- controllers/restController.js
+++ controllers/restController.js
@@ -8,52 +8,49 @@ const Comment = db.Comment;
 const User = db.User;
 
 const restController = {
-  //前台瀏覽餐廳-總表
   getRestaurants: (req, res) => {
-    let offset = 0;
+    let offset = 0; // 偏移幾筆後“開始”算
     let whereQuery = {};
     let categoryId = "";
+
     if (req.query.page) {
       offset = (req.query.page - 1) * pageLimit;
     }
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId);
       whereQuery["CategoryId"] = categoryId;
     }
-    Restaurant.findAll({
+
+    Restaurant.findAndCountAll({
       include: Category,
       where: whereQuery,
-      offset: offset,
+      nest: true,
+      raw: true,
+      offset,
       limit: pageLimit
     }).then(result => {
-      // data for pagination
       let page = Number(req.query.page) || 1; // 邏輯運算子 MDN https://mzl.la/2vKwE2d
       let pages = Math.ceil(result.count / pageLimit);
       let totalPage = Array.from({ length: pages }).map(
-        (item, index) => index + 1
+        (item, index) => index + 1 // array是給handlebars的each-helper用的，產生分頁按鈕的數字
       );
-      let prev = page - 1 < 1 ? 1 : page - 1;
+      let prev = page - 1 < 1 ? 1 : page - 1; //三元運算子 MDN https://mzl.la/2HK5iff
       let next = page + 1 > pages ? pages : page + 1;
-      // clean up restaurant data
-      const data = result.map(r => ({
-        ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50)
+      const data = result.rows.map(r => ({
+        ...r, //spread operator
+        description: r.description.substring(0, 50)
       }));
-      Category.findAll().then(categories => {
-        return res.render(
-          "restaurants",
-          JSON.parse(
-            JSON.stringify({
-              restaurants: data,
-              categories: categories,
-              categoryId: categoryId,
-              page: page,
-              totalPage: totalPage,
-              prev: prev,
-              next: next
-            })
-          )
-        );
+      Category.findAll({ raw: true }).then(categories => {
+        return res.render("restaurants", {
+          restaurants: data,
+          categories,
+          categoryId,
+          page,
+          totalPage,
+          prev,
+          next
+        });
       });
     });
   },
@@ -80,34 +77,62 @@ module.exports = restController;
 // const db = require("../models");
 // const Restaurant = db.Restaurant;
 // const Category = db.Category;
+// // 餐廳列表分頁功能
+// const pageLimit = 10;
+// // 瀏覽評論分頁功能
+// const Comment = db.Comment;
+// const User = db.User;
+
 // const restController = {
 //   //前台瀏覽餐廳-總表
 //   getRestaurants: (req, res) => {
-//     Restaurant.findAll({ include: Category }).then(restaurants => {
-//       const data = restaurants.map(r => ({
+//     let offset = 0;
+//     let whereQuery = {};
+//     let categoryId = "";
+//     if (req.query.page) {
+//       offset = (req.query.page - 1) * pageLimit;
+//     }
+//     if (req.query.categoryId) {
+//       categoryId = Number(req.query.categoryId);
+//       whereQuery["CategoryId"] = categoryId;
+//     }
+//     Restaurant.findAll({
+//       include: Category,
+//       where: whereQuery,
+//       offset: offset,
+//       limit: pageLimit
+//     }).then(result => {
+//       // data for pagination
+//       let page = Number(req.query.page) || 1; // 邏輯運算子 MDN https://mzl.la/2vKwE2d
+//       let pages = Math.ceil(result.count / pageLimit);
+//       let totalPage = Array.from({ length: pages }).map(
+//         (item, index) => index + 1
+//       );
+//       let prev = page - 1 < 1 ? 1 : page - 1;
+//       let next = page + 1 > pages ? pages : page + 1;
+//       // clean up restaurant data
+//       const data = result.map(r => ({
 //         ...r.dataValues,
 //         description: r.dataValues.description.substring(0, 50)
 //       }));
-
-//       Category.findAll().then(categories => {
-//         // 取出 categories
-//         return res.render(
-//           "restaurants",
-//           JSON.parse(
-//             JSON.stringify({
-//               restaurants: data,
-//               categories: categories,
-//               categoryId: categoryId
-//             })
-//           )
-//         );
+//       Category.findAll({ raw: true, nest: true }).then(categories => {
+//         return res.render("restaurants", {
+//           restaurants: data,
+//           categories: categories,
+//           categoryId: categoryId,
+//           page: page,
+//           totalPage: totalPage,
+//           prev: prev,
+//           next: next
+//         });
 //       });
 //     });
 //   },
-//   // 前台瀏覽餐廳-個別資料
 //   getRestaurant: (req, res) => {
 //     return Restaurant.findByPk(req.params.id, {
-//       include: Category
+//       // 解法： Eager Loading-https://sequelize.org/master/manual/eager-loading.html
+//       include: [Category, { model: Comment, include: [User] }]
+//       // include: [Category, Comment]
 //     }).then(restaurant => {
 //       return res.render(
 //         "restaurant",
@@ -120,4 +145,50 @@ module.exports = restController;
 //     });
 //   }
 // };
+
 // module.exports = restController;
+
+// // const db = require("../models");
+// // const Restaurant = db.Restaurant;
+// // const Category = db.Category;
+// // const restController = {
+// //   //前台瀏覽餐廳-總表
+// //   getRestaurants: (req, res) => {
+// //     Restaurant.findAll({ include: Category }).then(restaurants => {
+// //       const data = restaurants.map(r => ({
+// //         ...r.dataValues,
+// //         description: r.dataValues.description.substring(0, 50)
+// //       }));
+
+// //       Category.findAll().then(categories => {
+// //         // 取出 categories
+// //         return res.render(
+// //           "restaurants",
+// //           JSON.parse(
+// //             JSON.stringify({
+// //               restaurants: data,
+// //               categories: categories,
+// //               categoryId: categoryId
+// //             })
+// //           )
+// //         );
+// //       });
+// //     });
+// //   },
+// //   // 前台瀏覽餐廳-個別資料
+// //   getRestaurant: (req, res) => {
+// //     return Restaurant.findByPk(req.params.id, {
+// //       include: Category
+// //     }).then(restaurant => {
+// //       return res.render(
+// //         "restaurant",
+// //         JSON.parse(
+// //           JSON.stringify({
+// //             restaurant: restaurant
+// //           })
+// //         )
+// //       );
+// //     });
+// //   }
+// // };
+// // module.exports = restController;
--- seeders/20200216072439-my-seed-file.js
+++ seeders/20200216072439-my-seed-file.js
@@ -14,7 +14,8 @@ module.exports = {
           isAdmin: true,
           name: "root",
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
+          image: faker.image.avatar()
         },
         {
           email: "user1@example.com",
@@ -22,7 +23,8 @@ module.exports = {
           isAdmin: false,
           name: "user1",
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
+          image: faker.image.avatar()
         },
         {
           email: "user2@example.com",
@@ -30,7 +32,8 @@ module.exports = {
           isAdmin: false,
           name: "user2",
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
+          image: faker.image.avatar()
         }
       ],
       {}
