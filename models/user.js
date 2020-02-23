91571b343c683a38e56f17a3ecec65c1e406f731
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-02-23 20:47:30 +0800

    Create fellowship model and add followers and followings to users

--- models/user.js
+++ models/user.js
@@ -24,6 +24,24 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "UserId",
       as: "LikedRestaurants"
     });
+
+    // 定義資料模型：Followship
+    // 這邊因為是自關聯的緣故，所以看起來很像在念繞口令，但只要掌握兩個原則即可，假設我的 userId 是 5：
+    // 1. 找出所有 followingId 是 5 的人，就是我的 follower
+    // 2. 找出所有 followerId 是 5 的人，就是我在 following 的人
+
+    // 1.
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: "followingId",
+      as: "Followers"
+    });
+    // 2.
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: "followerId",
+      as: "Followings"
+    });
   };
   return User;
 };
